f76e2755b0f6f8cd8c1dc6b1d618b20d41812daf
Chrsteffen <Chrsteffen1@gmail.com>
2022-11-30 11:54:46 -0500

    stub up pages

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import NavBar from './Components/NavBar/NavBar';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='NavBar'>
+        <NavBar />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,9 @@
+const NavBar = () => {
+  return (  
+    <>
+      <h1>Star Wars</h1>
+    </>
+  );
+}
+ 
+export default NavBar;
